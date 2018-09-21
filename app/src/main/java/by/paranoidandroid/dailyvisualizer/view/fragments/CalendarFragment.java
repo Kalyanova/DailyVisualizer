3e7287633d8bfe450a36e3a82ffc3857660c41d5
Kseniya Lyanova <kalyan0va0622@gmail.com>
2018-09-22 02:58:08 +0300

    Day of week transfer.

--- app/src/main/java/by/paranoidandroid/dailyvisualizer/view/fragments/CalendarFragment.java
+++ app/src/main/java/by/paranoidandroid/dailyvisualizer/view/fragments/CalendarFragment.java
@@ -13,6 +13,8 @@ import androidx.fragment.app.Fragment;
 import by.paranoidandroid.dailyvisualizer.R;
 import by.paranoidandroid.dailyvisualizer.model.utils.CalendarDate;
 
+import static java.util.Calendar.DAY_OF_WEEK;
+
 public class CalendarFragment extends Fragment {
     private CalendarDate selectedDate;
     private CalendarListener calendarListener;
@@ -40,20 +42,21 @@ public class CalendarFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_calendar, container, false);
 
+        Calendar calendar = Calendar.getInstance();
         CalendarView calendarView = view.findViewById(R.id.calendar_view);
         // We open DayFragment only after the second click on selected date.
         calendarView.setOnDateChangeListener((cv, year, month, dayOfMonth) -> {
                     CalendarDate newSelectedDate = new CalendarDate(year, month, dayOfMonth);
                     if (selectedDate != null && selectedDate.equals(newSelectedDate)) {
-                        // TODO: pass also day of week
-                        calendarListener.onDaySelected(selectedDate, 1);
+                        calendar.set(year, month, dayOfMonth);
+                        int dayOfWeek = calendar.get(DAY_OF_WEEK);
+                        calendarListener.onDaySelected(selectedDate, dayOfWeek);
                     }
                     selectedDate = newSelectedDate;
                 }
         );
 
         if (selectedDate != null) {
-            Calendar calendar = Calendar.getInstance();
             calendar.set(selectedDate.year,
                     selectedDate.month,
                     selectedDate.dayOfMonth);
